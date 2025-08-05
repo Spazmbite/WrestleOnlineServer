65eff1e9cb2ff6731f92161228044903973c52b0
Spazmbite <spazmbite@gmail.com>
2025-08-05 22:11:55 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+let players = {}; // tu trzymamy dane o użytkownikach
+let bullets = {};
+
+
+var countBullets = 0;
+
+app.use(express.static('public')); // frontend w folderze 'public'
+
+io.on('connection', (socket) => {
+  console.log('Użytkownik połączony:', socket.id);
+
+  // Kiedy klient wyśle swoje koordynaty
+  socket.on('sendCoords', (coords) => {
+    if(Array.isArray(coords)){
+      players[socket.id] = coords[0];
+      for(var p = 1 ; p < coords.length ; p++){
+        players[socket.id+"_"+p] = coords[p];
+      }
+    } else {
+      players[socket.id] = coords;
+    }
+    
+    //io.emit('updateCoords', players); // wysyłamy do wszystkich
+  });
+
+
+
+  /* BACKUP
+  // Kiedy klient wyśle swoje koordynaty
+  socket.on('sendCoords', (coords) => {
+    players[socket.id] = coords;
+    io.emit('updateCoords', players); // wysyłamy do wszystkich
+  });
+  */
+
+  // Kiedy klient wyśle info o strzale
+  socket.on('sendBullets', (bullet) => {
+    //bullets[socket.id] = bullet;
+    //countBullets++;
+    //bullets.push(bullet);
+    io.emit('updateBullets', bullet); // wysyłamy do wszystkich
+  });
+
+  // Kiedy klient wyśle info log
+  socket.on('sendLog', (log) => {
+    io.emit('updateLog', log); // wysyłamy do wszystkich
+  });
+
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+    /*if(players[socket.id+"_1"]){ delete players[socket.id+"_1"]; }
+    if(players[socket.id+"_2"]){ delete players[socket.id+"_2"]; }
+    if(players[socket.id+"_3"]){ delete players[socket.id+"_3"]; }
+    if(players[socket.id+"_4"]){ delete players[socket.id+"_4"]; }
+    if(players[socket.id+"_5"]){ delete players[socket.id+"_5"]; }*/
+    io.emit('updateCoords', players);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Serwer działa na porcie ${PORT}`);
+});
+
+setInterval(function(){
+  io.emit('updateCoords', players);
+  //io.emit('updateBullets', bullets);
+  //bullets = [];
+},20);
+
+setInterval(function(){
+  if(countBullets > 0){ console.log(countBullets); }
+  countBullets = 0;
+},1000);
+
+setInterval(() => {
+  fetch("https://wrestleonline.onrender.com")
+    .then(() => console.log("Ping!"))
+    .catch(err => console.error("Błąd pingu:", err));
+}, 10 * 60 * 1000); // co 10 minut
