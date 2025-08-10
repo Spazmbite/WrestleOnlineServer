cc24978c18f7f6bb042977998be1ea800fe51fae
Spazmbite <spazmbite@gmail.com>
2025-08-10 14:53:58 +0200

    Update server.js 10.8.25 14.53

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
         players[newCoords.socket].anim = players[newCoords.socket].forceanim;
         players[newCoords.socket].animtime = 0;
         //io.to(newCoords.socket).emit('updatePlayers', players);
-        //io.emit('updatePlayers', players);
+        io.emit('updatePlayers', players);
         
       } else {
         players[newCoords.socket].anim = newCoords.anim;
