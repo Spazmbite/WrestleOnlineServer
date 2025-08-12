ede9edbf40c1ae21bee048046f718d5e33b4d518
Spazmbite <spazmbite@gmail.com>
2025-08-12 23:43:33 +0200

    Update server.js 12.8.25 23.43

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ io.on('connection', (socket) => {
     if(players[newCoords.socket] != undefined){
 
       players[newCoords.socket].timeout = Date.now();
-      if(players[newCoords.socket].controlledBy != null && newCoords.socket == socket.id){ return; }
+      //if(players[newCoords.socket].controlledBy != null && newCoords.socket == socket.id){ return; }
 
-      if(players[newCoords.socket].controlledBy){ console.log(socket.id,newCoords.socket) }
+      //if(players[newCoords.socket].controlledBy){ console.log(socket.id,newCoords.socket) }
       
       players[newCoords.socket].x = newCoords.x;
       players[newCoords.socket].y = newCoords.y;
